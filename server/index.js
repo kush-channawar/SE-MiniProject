5a868b1a4f5ad9f065d5a4ba355c61aa37d17740
kush-channawar <channawarkush@gmail.com>
2021-04-17 21:52:01 +0530

    add students and teacher from csv to db

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
+var fs = require('fs');
+var csv = require('fast-csv');
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require('path')
+const pool = require('./db');
 const fileUpload = require('express-fileupload');
 app.use(fileUpload());
 app.use(cors());
 app.use(express.json());
 
 app.use(express.static('public'));
+
+pool.connect(function(err){
+  if(err)
+  {
+      console.log(err);
+  }
+});
+
+let counter = 0; 
+
 app.post('/dashboardadmin/uploadstudent', (req, res) => {
     if (req.files === null) {
       return res.status(400).json({ msg: 'No file uploaded' });
@@ -22,6 +35,35 @@ app.post('/dashboardadmin/uploadstudent', (req, res) => {
       }
   
       res.json({ fileName: file.name, filePath: `/uploads/student/${file.name}` });
+
+
+
+      let csvStream = csv.parseFile(`${reqPath}/client/public/uploads/student/${file.name}`, { headers: true })
+    .on("data", function(record){
+        csvStream.pause();
+
+        if(counter < 100)
+        {
+            let id = record.MIS;
+            let name = record.Name;
+            let email = record.Email;
+            let password = record.Password;
+            pool.query("INSERT INTO student(user_id, user_name, user_email, user_password) VALUES($1, $2, $3, $4)", [id, name, email,password], function(err){
+                if(err)
+                {
+                    console.log(err);
+                }
+            });
+            ++counter;
+        }
+
+        csvStream.resume();
+
+    }).on("end", function(){
+        console.log("Job is done!");
+    }).on("error", function(err){
+        console.log(err);
+    });
     });
   });
   
@@ -39,6 +81,32 @@ app.post('/dashboardadmin/uploadstudent', (req, res) => {
       }
   
       res.json({ fileName: file.name, filePath: `/uploads/teacher/${file.name}` });
+      let csvStream = csv.parseFile(`${reqPath}/client/public/uploads/teacher/${file.name}`, { headers: true })
+    .on("data", function(record){
+        csvStream.pause();
+
+        if(counter < 100)
+        {
+            let id = record.MIS;
+            let name = record.Name;
+            let email = record.Email;
+            let password = record.Password;
+            pool.query("INSERT INTO teacher(user_id, user_name, user_email, user_password) VALUES($1, $2, $3, $4)", [id, name, email,password], function(err){
+                if(err)
+                {
+                    console.log(err);
+                }
+            });
+            ++counter;
+        }
+
+        csvStream.resume();
+
+    }).on("end", function(){
+        console.log("Job is done!");
+    }).on("error", function(err){
+        console.log(err);
+    });
     });
   });
   
